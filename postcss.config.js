770987a4b350bbbf55897bd5a1040ffb2c01248b
Mat Moore <matmoore@users.noreply.github.com>
2020-03-30 22:07:08 +0100

    Get TailwindCSS working properly
    This still shows a dummy form, but now it's styled

--- postcss.config.js
+++ postcss.config.js
@@ -7,8 +7,8 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
-    require('tailwindcss'),
+    }),
+    require('tailwindcss')('./app/javascript/css/tailwind.js'),
     require('autoprefixer'),
   ]
 }
