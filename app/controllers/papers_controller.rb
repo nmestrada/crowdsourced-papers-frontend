3ce5f5de71ce9426700e957eb350425a3b9f621f
Mat Moore <matmoore@users.noreply.github.com>
2020-04-25 23:06:48 +0100

    Add a distinct admin layout, with a log out button
    This is currently unstyled.

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,6 +1,7 @@
 class PapersController < ApplicationController
     protect_from_forgery
     before_action :authenticate_user!, only: [:new, :create]
+    layout "admin", only: [:new, :create]
 
     def new
         @paper = Paper.new
